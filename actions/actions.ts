ab15a64c01a79bcb7afdc47bf7f1d6e57b510fba
mohammadtanim24h <mohammad.tanim24h@gmail.com>
2026-03-17 13:20:31 +0600

    Added error handling for createPost action

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,23 +1,33 @@
 "use server";
 
+import { Prisma } from "@/app/generated/prisma/client";
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
 
 // a server action is just a function that runs on the server. This function below is a server action. The "use server" at the top of the file tells Next.js that this file contains server actions.
 export async function createPost(data: FormData) {
-    const title = data.get("title") as string;
-    const content = data.get("content") as string;
-    const slug = title.toLowerCase().replace(/\s+/g, "-");
+    try {
+        const title = data.get("title") as string;
+        const content = data.get("content") as string;
+        const slug = title.toLowerCase().replace(/\s+/g, "-");
 
-    // create post in database
-    await prisma.post.create({
-        data: {
-            title,
-            content,
-            slug,
-            authorId: 1,
-        },
-    });
+        // create post in database
+        await prisma.post.create({
+            data: {
+                title,
+                content,
+                slug,
+                authorId: 2,
+            },
+        });
+    } catch (error) {
+        console.error("Error creating post:", error);
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2002") {
+                console.error("There is a unique constraint violation.");
+            }
+        }
+    }
 
     revalidatePath("/posts"); // revalidate the /posts page to show the new post
 }
